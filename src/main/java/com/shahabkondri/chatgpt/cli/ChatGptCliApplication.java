be0cf4654709319028e39984c9be54ef8b35919f
Shahab Kondri <shahab.kondri@gmail.com>
2023-03-20 22:44:00 -0400

    rename `application.properties` to `chat-gpt-cli.config`

--- src/main/java/com/shahabkondri/chatgpt/cli/ChatGptCliApplication.java
+++ src/main/java/com/shahabkondri/chatgpt/cli/ChatGptCliApplication.java
@@ -5,20 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
-import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Shahab Kondri
  */
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@PropertySource("classpath:chat-gpt-cli.config")
 public class ChatGptCliApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ChatGptCliApplication.class);
 		app.setBannerMode(Banner.Mode.OFF);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "0"));
+		app.setDefaultProperties(Map.of("server.port", "0"));
 		ConfigurableApplicationContext context = app.run(args);
 		SpringApplication.exit(context);
 	}
