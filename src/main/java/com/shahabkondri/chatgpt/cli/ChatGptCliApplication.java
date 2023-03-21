a59fbe491c9efe13518243907d185320e79813ee
Shahab Kondri <shahab.kondri@gmail.com>
2023-03-20 22:01:40 -0400

    run on a random port;

--- src/main/java/com/shahabkondri/chatgpt/cli/ChatGptCliApplication.java
+++ src/main/java/com/shahabkondri/chatgpt/cli/ChatGptCliApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Collections;
 
 /**
  * @author Shahab Kondri
@@ -15,7 +18,9 @@ public class ChatGptCliApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ChatGptCliApplication.class);
 		app.setBannerMode(Banner.Mode.OFF);
-		SpringApplication.exit(app.run());
+		app.setDefaultProperties(Collections.singletonMap("server.port", "0"));
+		ConfigurableApplicationContext context = app.run(args);
+		SpringApplication.exit(context);
 	}
 
 }
